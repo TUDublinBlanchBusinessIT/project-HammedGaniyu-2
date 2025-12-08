2594bd8bce62d1aed92414b9be320c0b663db78e
HammedGaniyu <B00167256@mytudublin.ie>
2025-12-08 15:14:55 +0000

    Insert php fixed to match

--- insert.php
+++ insert.php
@@ -6,15 +6,8 @@ $brand = $_POST['brand'];
 $model = $_POST['model'];
 
 
-$sql = "INSERT INTO sneakers (brand, model) VALUES ('$brand', '$model')";
+$sql = "INSERT INTO sneakers (brand_id, model) VALUES ('$brand_id', '$model')";
+mysqli_query($conn, $sql)
 
-if ($conn->query($sql) === TRUE) {
-    echo "Sneaker added successfully!<br>";
-    echo "<a href='index.php'>Add another sneaker</a><br>";
-    echo "<a href='display.php'>View sneaker list</a>";
-} else {
-    echo "Error inserting sneaker: " . $conn->error;
-}
-
-$conn->close();
+echo "Sneaker Added"
 ?>
